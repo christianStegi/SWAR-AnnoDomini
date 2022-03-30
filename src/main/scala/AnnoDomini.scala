3517ec89b6115d9c6b20deff303d39e2bfabeabc
Sarah Tiefert <sarah.tiefert@htwg-konstanz.de>
2022-03-30 09:53:02 +0200

    Renamed AnoDomini into AnnoDomini, the former is a typo added overview of missing SE functionality in AnnoDomini

--- src/main/scala/AnnoDomini.scala
+++ src/main/scala/AnnoDomini.scala
@@ -25,12 +25,24 @@ def correctNumber(num: Int) : 2 | 3 | 4 = {
     case _ => 3
     // TODO: check if this can be simplyfied with higher functions
 }
+
+// general things to do:
+// Missing functionality form SE
+// reintegrade Continuous deployment with Travis (need new Travis Account) (HA 6)
+// add Design Patterns (HA 7)
+// add Undo and Redo (HA 8)
+// add Gui (HA 9)
+// Add Interfaces (HA 10 )
+// do Dependency Injection (HA 11)
+// Add method to save game (HA 12 - File IO)
+// Add Docker (HA 13)
+
 // TODO: add method to check numbers of Players
 // TODO: add method to view all players at once
 // TODO: add better Input errorhandling
 
 
-object AnoDomini {
+object AnnoDomini {
 
   val controller = new Controller(TableGenerator().createTable)
   val tui = new Tui(controller)
