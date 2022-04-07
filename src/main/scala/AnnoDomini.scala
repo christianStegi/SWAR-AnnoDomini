ebd9b60a9b324de9bd6503cec7f75b63ac7366bd
Sarah Tiefert <sarah.tiefert@htwg-konstanz.de>
2022-04-07 11:32:12 +0200

    Cleaned up AnnoDomini main. Now all the Input is handled by the TUI

--- src/main/scala/AnnoDomini.scala
+++ src/main/scala/AnnoDomini.scala
@@ -7,24 +7,6 @@ import view.Tui
 import scala.io.StdIn.readLine
 
 
-def NumberOfPlayers(num: Int) : 2 | 3 | 4 ={
-  num match
-    case 2 => 2
-    case 3 => 3
-    case 4 => 4
-    case _ => correctNumber(num)
-}
-
-def correctNumber(num: Int) : 2 | 3 | 4 = {
-  var returnNum = 3
-  if  (num <= 2)  returnNum = 2
-  else if (num >= 4) returnNum = 4
-  returnNum match
-    case 2 => 2
-    case 4 => 4
-    case _ => 3
-    // TODO: check if this can be simplyfied with higher functions
-}
 
 // general things to do:
 // Missing functionality form SE
@@ -50,34 +32,6 @@ object AnnoDomini {
 
   // val scanner = new Scanner(System.in)
 
-  def main(args:Array[String]):Unit = 
-
-    println("Willkommen bei AnoDomini!")
-    // create scanner for player input
-    var input: String = ""
-
-    // ask for prefered deck (start with default random deck)
-
-    // generate deck
-
-    // ask for amount of players
-    println("how many players? (2, 3 or 4)")
-    input = readLine
-    val numPlayers: 2 | 3 | 4 = NumberOfPlayers(input.toInt)
-    controller.createTestTable(numPlayers)
-    // if only one player: switch into Singleplayer mode
-    // not implemented yet
-
-    while(input != "q")
-    do 
-      // show table + current player
-      tui.giveOptions()
-      input = readLine()
-      println("input is: " + input)
-      tui.processInputLine(input)
-
-      // check if player has won (no more cards in players hand)
-      tui.confirmWinner()
-    // TODO: change it to a recusion
-
+  def main(args:Array[String]):Unit =
+    tui.startGame()
 }
